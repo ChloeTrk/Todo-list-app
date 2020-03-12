29173641b84924cb9cb2b8771624c1d91567f597
Chloe <chloeturchi@gmail.com>
2020-03-12 18:02:57 +0100

    comment learn.json

--- js/store.js
+++ js/store.js
@@ -74,47 +74,29 @@
       const data = JSON.parse(localStorage[this._dbName]);
       const { todos } = data;
       callback = callback || function () { };
-      // Generate an ID
-      let newId = '';
-      const charset = '0123456789';
-      for (let i = 0; i < 6; i++) {
-        newId += charset.charAt(Math.floor(Math.random() * charset.length));
-      }
-
-      // Check if ID already exist
-      const todo = todos.find(todo => todo.id === newId)
-      if (todo !== undefined){
-        newId = '';
-        for (let j = 0; j < 6; j++) {
-          newId += charset.charAt(Math.floor(Math.random() * charset.length));
-        }
-      }
       
       // If an ID was actually given, find the item and update each property
       if (id) {
-        const todo = todos.find(todo => todo.id === id)
-        for (const key in updateData) {
-          todo[key] = updateData[key];
-        } 
-   
-        // for (let k = 0; k < todos.length; k++) {
-        //   if (todos[k].id === id) {
-        //     for (const key in updateData) {
-        //       todos[k][key] = updateData[key];
-        //     }
-        //     break;
-        //   }
-        // }
-
+        for (var i = 0; i < todos.length; i++) {
+          if (todos[i].id === id) {
+            for (var key in updateData) {
+              todos[i][key] = updateData[key];
+            }
+            break;
+          }
+        }
         localStorage[this._dbName] = JSON.stringify(data);
         callback.call(this, todos);
       } else {
+
         // Assign an ID
-        updateData.id = parseInt(newId);
+        updateData.id = Date.now();
+        console.log(updateData.id)
         todos.push(updateData);
         localStorage[this._dbName] = JSON.stringify(data);
         callback.call(this, [updateData]);
       }
+
     }
 
     /**
@@ -126,17 +108,13 @@
     remove(id, callback) {
       const data = JSON.parse(localStorage[this._dbName]);
       const { todos } = data;
-      let todoId;
+
       for (let i = 0; i < todos.length; i++) {
         if (todos[i].id === id) {
-          todoId = todos[i].id;
+          todos.splice(i, 1);
         }
       }
-      for (let j = 0; j < todos.length; j++) {
-        if (todos[j].id === todoId) {
-          todos.splice(j, 1);
-        }
-      }
+
       localStorage[this._dbName] = JSON.stringify(data);
       callback.call(this, todos);
     }
